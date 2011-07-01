dabca22aed833f29046c04cbc408a826df0e3226
wjjeon <wjjeon@gmail.com>
2011-07-01 05:19:05 +0000

    Copyright notice added

--- Source/WebCore/bindings/js/JSDOMWindowCustom.cpp
+++ Source/WebCore/bindings/js/JSDOMWindowCustom.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2011 Samsung Electronics Corporation. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
